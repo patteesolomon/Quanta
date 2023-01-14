6fdc48bcf9d15fd3c7ae7062ff4e8f854eaab477
patteesolomon <theshiningkillerrashakublade@outlook.com>
2023-01-13 23:35:48 -0600

    update IdCellLight_Core.js

--- IdCellLight_Core.js
+++ IdCellLight_Core.js
@@ -2,7 +2,13 @@ import React from 'react.js';
 
 function StartCore(){
   var j = 7;
-  let state = '';
+  let state = ['check','main',"semi",'redo','execute'];
+  
+  switch(state)
+  {
+    case()
+    
+  }
 }
 
 export default StartCore
